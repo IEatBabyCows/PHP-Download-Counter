6051114abf55fdf9df1563a6c763527478a77170
IEatBabyCows <IEBCASITM@gmail.com>
2014-02-10 20:25:47 +1030

    CSS moved into a stylesheet

--- www/stats/index.php
+++ www/stats/index.php
@@ -6,22 +6,16 @@ echo "<!DOCTYPE html>\n";
 echo "<html>\n";
   echo "<head>\n";
     echo "<title>Download Stats</title>\n";
-    echo "<style type=\"text/css\">\n"; 
-      echo ".tg  {border-collapse:collapse;border-spacing:0;border-color:#999;}\n"; 
-      echo ".tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#999;color:#444;background-color:#F7FDFA;}\n"; 
-      echo ".tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;border-color:#999;color:#fff;background-color:#26ADE4;}\n"; 
-      echo ".tg .tg-s6z2{text-align:center}\n"; 
-      echo ".tg .tg-hgcj{font-weight:bold;text-align:center}\n"; 
-    echo "</style>\n"; 
+    echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"../css/style.css\">\n";
   echo "</head>\n";
 
   echo "<body>\n";
     echo "<table>\n";
       echo "<table class=\"tg\">\n"; 
-      echo "  <tr>\n"; 
-      echo "    <th class=\"tg-hgcj\">FILE</th>\n"; 
-      echo "    <th class=\"tg-hgcj\">DOWNLOADS</th>\n"; 
-      echo "  </tr>\n"; 
+      echo "<tr>\n"; 
+      echo "<th class=\"tg-head\">FILE</th>\n"; 
+      echo "<th class=\"tg-head\">DOWNLOADS</th>\n"; 
+      echo "</tr>\n"; 
 
   foreach ($files as $file) {
     if (substr($file, -4) === ".log") {
@@ -37,10 +31,10 @@ echo "<html>\n";
         fclose($handle);
         $totalLines = $totalLines - 1; //This method counts the last empty line
       }
-      echo "  <tr>\n"; 
-      echo "    <td class=\"tg-s6z2\">" . substr($file, 0, -4) . "</td>\n"; 
-      echo "    <td class=\"tg-s6z2\">" . $totalLines . "</td>\n"; 
-      echo "  </tr>\n"; 
+      echo "<tr>\n"; 
+      echo "<td class=\"tg-body\">" . substr($file, 0, -4) . "</td>\n"; 
+      echo "<td class=\"tg-body\">" . $totalLines . "</td>\n"; 
+      echo "</tr>\n"; 
     }
   }
     echo "</table>\n";
